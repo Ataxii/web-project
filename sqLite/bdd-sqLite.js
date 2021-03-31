3e7c68dd8a37703560797937920c34604f6bbf99
o19012876 <badr.ouaddah@etu.univ-amu.fr>
2021-03-31 15:01:30 +0200

    table sql

---
+++ sqLite/bdd-sqLite.js
@@ -0,0 +1,23 @@
+var sqlite = require('better-sqlite3');
+var db = new sqlite.Database('main.db', err =>{
+    if (err)
+        throw err
+    console.log('Databse started on ' + db)
+})
+
+db.close(err => {
+    if (err)
+        throw err
+    console('Datebase closed.')
+})
+
+db.prepare('DROP TABLE IF EXISTS userLogin').run();
+db.prepare('DROP TABLE IF EXISTS userProfil').run();
+
+db.prepare('CREATE TABLE userLogin (id INTEGER PRIMARY KEY, pseudo TEXT, password TEXT)').run();
+db.prepare('CREATE TABLE userProfil (id INTEGER PRIMARY KEY, photo_de_profil TEXT, biographie TEXT, etudes TEXT, contact TEXT)').run();
+
+
+
+
+
